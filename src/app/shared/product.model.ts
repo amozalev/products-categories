2bc2352896239b0dbd9b485e5386e12cabcb4de8
amozalev <mozalev.alexey@gmail.com>
2018-12-15 20:05:06 +0300

    Small improvement.

--- src/app/shared/product.model.ts
+++ src/app/shared/product.model.ts
@@ -4,14 +4,16 @@ export class Product {
   price: number;
   description: string;
   picture: string;
+  category: string;
   rating: number;
 
-  constructor(id: number, title: string, price: number, description: string, picture: string) {
+  constructor(id: number, title: string, price: number, description: string, picture: string, category: string) {
     this.id = id;
     this.title = title;
     this.price = price;
     this.description = description;
     this.picture = picture;
+    this.category = category;
   }
 
 }
