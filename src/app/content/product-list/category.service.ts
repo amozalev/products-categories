0ef60016ec51bf40db4b94d87ffc6dac5e44e80f
amozalev <mozalev.alexey@gmail.com>
2019-06-06 18:54:51 +0300

    Children categories are grouped in a parent one in a final categories list (draft).

--- src/app/content/product-list/category.service.ts
+++ src/app/content/product-list/category.service.ts
@@ -1,20 +1,63 @@
 import {Category} from '../../shared/category.model';
+import index from '@angular/cli/lib/cli';
 
 export class CategoryService {
+  // final_categories: { [id: number]: { category: Category }[] }[];
+  final_categories: any[] = [];
   private categories: Category[] = [
     new Category(
       0,
       'fruit',
-      'Fruits'),
+      'Fruits',
+      null,
+      []),
     new Category(
       1,
       'vegetable',
-      'Vegetables'
+      'Vegetables',
+      null,
+      []
+    ),
+    new Category(
+      2,
+      'tropical_fruit',
+      'Tropical fruits',
+      0,
+      []
     )
   ];
 
   getCategories() {
-    return this.categories.slice();
+    this.categories.slice().forEach((val) => {
+
+      if (val.parent != null) {
+        const parent_category = this.categories[val.parent];
+
+        const _index = this.final_categories.findIndex((category) => {
+          if (parent_category.id === category.id) {
+            return category;
+          }
+        });
+        if (_index === -1) {
+          this.final_categories.push(parent_category);
+        }
+        this.final_categories[_index].children.push(val);
+
+      } else {
+        const _index = this.final_categories.findIndex((category) => {
+          if (val.id === category.id) {
+            return category;
+          }
+        });
+        if (_index === -1) {
+          this.final_categories.push(val);
+        }
+      }
+
+    });
+    console.log('final_categories: ', this.final_categories);
+
+    return this.final_categories;
   }
 
   getCategoryNameById(cat_id: number) {
--- src/app/shared/category.model.ts
+++ src/app/shared/category.model.ts
@@ -3,12 +3,14 @@ export class Category {
   name: string;
   normal_name: string;
   parent: number;
+  children: number[];
 
-  constructor(id: number, name: string, normal_name: string, parent = null) {
+  constructor(id: number, name: string, normal_name: string, parent = null, children: number[] = null) {
     this.id = id;
     this.name = name;
     this.normal_name = normal_name;
     this.parent = parent;
+    this.children = children;
   }
 
 }
