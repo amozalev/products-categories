ac6fdafb60689b6a521d61d9fc941c1985a28cdc
amozalev <mozalev.alexey@gmail.com>
2019-06-12 18:58:52 +0300

    "skip" and "limit" are added.

--- server/myapp/api_v1/rest_models/restCategory.py
+++ server/myapp/api_v1/rest_models/restCategory.py
@@ -1,16 +1,25 @@
 import json
 import bson
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from server.myapp.db_models.Category import Category as _Category
 
 
 class Category(Resource):
-    def get(self, category_id=None):
+    def get(self, category_id=None, skip: int = 0, limit: int = 10) -> json:
+        parser = reqparse.RequestParser()
+        parser.add_argument('skip', type=int)
+        parser.add_argument('limit', type=int)
+        args = parser.parse_args()
+        if args['skip'] is not None:
+            skip = args['skip']
+        if args['limit'] is not None:
+            limit = args['limit']
+
         if category_id is not None:
             categories = _Category.objects(_id=bson.ObjectId(category_id))
         else:
             categories = _Category.objects
-        data = json.loads(categories.to_json())
+        data = json.loads(categories.skip(skip).limit(limit).to_json())
         return {'data': data}
 
     def post(self):
--- server/myapp/api_v1/rest_models/restProduct.py
+++ server/myapp/api_v1/rest_models/restProduct.py
@@ -1,16 +1,25 @@
 import json
 import bson
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from server.myapp.db_models.Product import Product as _Product
 
 
 class Product(Resource):
-    def get(self, product_id=None):
+    def get(self, product_id=None, skip: int = 0, limit: int = 10) -> json:
+        parser = reqparse.RequestParser()
+        parser.add_argument('skip', type=int)
+        parser.add_argument('limit', type=int)
+        args = parser.parse_args()
+        if args['skip'] is not None:
+            skip = args['skip']
+        if args['limit'] is not None:
+            limit = args['limit']
+
         if product_id is not None:
             products = _Product.objects(_id=bson.ObjectId(product_id))
         else:
             products = _Product.objects
-        data = json.loads(products.to_json())
+        data = json.loads(products.skip(skip).limit(limit).to_json())
         return {'data': data}
 
     def post(self):
