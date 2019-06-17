aa20a1a4c6ab2066eda22d7a980b4b31be23aea6
amozalev <mozalev.alexey@gmail.com>
2019-06-17 18:32:16 +0300

    Models are corrected.

--- server/myapp/db_models/Category.py
+++ server/myapp/db_models/Category.py
@@ -4,4 +4,4 @@ from mongoengine import *
 class Category(Document):
     name = StringField(required=True)
     normal_name = StringField(required=True)
-    parent = StringField(required=True)
+    parent = ObjectIdField()
--- server/myapp/db_models/Product.py
+++ server/myapp/db_models/Product.py
@@ -3,10 +3,10 @@ from mongoengine import *
 
 class Product(Document):
     title = StringField(required=True)
+    price = DecimalField(required=True)
     description = StringField(required=True)
+    picture = URLField(required=True)
+    category = ObjectIdField(required=True)
     volume = FloatField(required=True)
     units = StringField(required=True)
     producer = StringField(required=True)
-    price = DecimalField(required=True)
-    picture = URLField(required=True)
-    category = ListField(required=True)
