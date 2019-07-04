796b4bcfd0d4ebaea16c68b591618e46dbe3a6ca
amozalev <mozalev.alexey@gmail.com>
2019-07-04 15:10:22 +0300

    Product item cards are improved.

--- client/src/app/content/product-item/product-item.component.ts
+++ client/src/app/content/product-item/product-item.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Product} from '../../shared/models/product.model';
 import {ProductService} from '../../services/product.service';
+import {AppConfig} from '../../app.config';
 
 @Component({
   selector: 'app-product-item',
@@ -8,12 +9,14 @@ import {ProductService} from '../../services/product.service';
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent implements OnInit {
+  currency: string;
   @Input() product: Product;
 
   constructor(private productService: ProductService) {
   }
 
   ngOnInit() {
+    this.currency = AppConfig.currency;
   }
 
   addToCart() {
