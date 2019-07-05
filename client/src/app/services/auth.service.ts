67045f9f86ed24481bda8aa8ba6da21e3a9a9660
amozalev <mozalev.alexey@gmail.com>
2019-07-06 00:12:58 +0300

    URL is fixed.

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -3,20 +3,29 @@ import {HttpClient} from '@angular/common/http';
 
 import {AppConfig} from '../app.config';
 import {shareReplay} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 
 @Injectable()
 export class AuthService {
+  private auth_token: string;
   isSignedIn = false;
+  isAuthorized = new Subject<boolean>();
 
   constructor(private httpService: HttpClient) {
   }
 
   login(email: string, password: string) {
-    return this.httpService.post(`${AppConfig.apiURL}/${AppConfig.apiURL}/auth/login`, {email, password}).pipe(
+    return this.httpService.post(`${AppConfig.apiURL}/${AppConfig.apiPrefix}/auth/login`, {email, password}).pipe(
       shareReplay()
     );
+  }
 
-    this.isSignedIn = true;
+  set_token(token: string) {
+    this.auth_token = token;
+  }
+
+  get_token() {
+    return this.auth_token;
   }
 }
