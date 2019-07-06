c1456346d773cdd8d6876d5062951f24754af12a
amozalev <mozalev.alexey@gmail.com>
2019-07-06 19:21:54 +0300

    Auth guard service and interceptor are implemented.

---
+++ client/src/app/auth/auth-guard.service.ts
@@ -0,0 +1,27 @@
+import {ActivatedRoute, ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {AuthService} from '../services/auth.service';
+
+@Injectable()
+export class AuthGuardService implements CanActivate, CanLoad {
+  constructor(private authService: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {
+
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
+  }
+
+  canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.isAuthenticated();
+  }
+}
+
